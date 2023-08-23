74b5e474ffd00423b00bafd31b5e59a521b50bce
Raheel Mansoor <raheelmnsr@gmail.com>
2023-08-23 19:16:14 +0500

    database tutorials

---
+++ database/migrations/2023_08_23_092140_create_students_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->integer('st_id')->autoIncrement();
+            $table->string('name',50);
+            $table->string('email',50)->unique();
+            $table->integer('age')->comment('age > 20');
+            $table->string('city',50)->default('peshawar');
+            $table->primary('st_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
